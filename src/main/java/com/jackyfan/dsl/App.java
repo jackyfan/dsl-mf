7abd0d4fd1ea04abb04b41c87843cdd87368d89b
jacky <jackyfan@163.com>
2025-06-16 16:31:32 +0800

    first commit

---
+++ src/main/java/com/jackyfan/dsl/App.java
@@ -0,0 +1,13 @@
+package com.jackyfan.dsl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
